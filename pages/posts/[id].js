f4f8ad998b95c23f2ff526003d8beb3ab829fbdd
youliangdao <youliangdaojing5@gmail.com>
2023-07-17 15:52:56 +0900

    ✨ 既存のブログ記事のページにアクセスできるようにする

---
+++ pages/posts/[id].js
@@ -0,0 +1,58 @@
+import { useRouter } from "next/router";
+import Layout from "../../components/Layout";
+import Link from "next/link";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export default function Post({ post }) {
+  const router = useRouter();
+  if (!post) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Layout title={post.title}>
+      <p className="m-4">
+        {"ID : "}
+        {post.id}
+      </p>
+      <p className="mb-4 text-xl font-bold">{post.title}</p>
+      <p className="mb-12">{post.created_at}</p>
+      <p className="px-10">{post.content}</p>
+      <Link href="/blog-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 mr-3"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            />
+          </svg>
+          <span>Back to blog-page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = await getAllPostIds();
+  return {
+    paths,
+    fallback: ,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const post = await getPostData(params.id);
+  return {
+    props: {
+      post,
+    },
+  };
+}
