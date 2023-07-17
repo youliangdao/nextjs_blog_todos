caf81103129ec172e427658c63d1543aa4042943
youliangdao <youliangdaojing5@gmail.com>
2023-07-17 13:12:27 +0900

    ✨ Authページ作成

--- components/Auth.js
+++ components/Auth.js
@@ -1,3 +1,145 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie();
+
 export default function Auth({ children }) {
-  return <></>;
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+
+  const login = async () => {
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create/`,
+        {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          } else if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          const options = { path: "/" };
+          cookie.set("access_token", data.access, options);
+        });
+      router.push("/main-page");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const authUser = async (e) => {
+    e.preventDefault();
+    if (isLogin) {
+      login();
+    } else {
+      try {
+        await fetch(`${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`, {
+          method: "POST",
+          body: JSON.stringify({ username: username, password: password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }).then((res) => {
+          if (res.status === 400) {
+            throw "authentication failed";
+          }
+        });
+        login();
+      } catch (error) {
+        alert(error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          className="mx-auto h-10 w-auto"
+          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          alt="Your Company"
+        />
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          {isLogin ? "Sign in to your account" : "Sign up for an account"}
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form
+          className="space-y-6"
+          action="#"
+          method="POST"
+          onSubmit={authUser}
+        >
+          <input type="hidden" name="remember" value="true" />
+          <div>
+            <div className="mt-2">
+              <input
+                name="username"
+                type="text"
+                autoComplete="username"
+                placeholder="username"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="mt-2">
+              <input
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                placeholder="password"
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              />
+            </div>
+          </div>
+          <div className="flex items-center justify-center">
+            <div className="text-sm">
+              <span
+                className="cursor-pointer font-semibold text-white hover:text-indigo-500 justify-center"
+                onClick={() => {
+                  setIsLogin(!isLogin);
+                }}
+              >
+                change mode?
+              </span>
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              {isLogin ? "Login with JWT" : "Create new user"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
