8b342c99be38cf48554b89b75b97d76ebae8373d
youliangdao <youliangdaojing5@gmail.com>
2023-07-17 14:46:26 +0900

    ✨ getStaticPropsを利用してブログ一覧ページを作成

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-export default function BlogPage() {
+export default function BlogPage({ filteredPosts }) {
   return (
     <Layout title="Blog page">
+      <ul>
+        {filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
       <Link href="/main-page">
         <div className="flex cursor-pointer mt-12">
           <svg
@@ -25,4 +31,11 @@ export default function BlogPage() {
       </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostsData();
+  return {
+    props: { filteredPosts },
+  };
 }
